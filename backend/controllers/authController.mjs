edafff828035e2a41700dd0d47bf4a9c84b8242a
Gourav <gouravgunaga555@gmail.com>
2024-03-20 21:47:57 +0530

    Able to add new user to the database

--- backend/controllers/authController.mjs
+++ backend/controllers/authController.mjs
@@ -1,6 +1,28 @@
+import { User } from "../models/userModel.mjs"
+export const signup = async (req, res) => {
+    try {
+        const { fullName, userName, password, confirPassWord, gender } = req.body
 
-export const signup = (req, res) => {
-    res.send("Sign Up page")
+        if (password !== confirPassWord) return res.status(400).json({ error: "Password don't match" })
+        if (await User.findOne({ username: userName })) return res.status(400).json({ error: "userrName already taken" })
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`
+
+        const createdUser = new User({
+            fullName,
+            username: userName,
+            password,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+        const newUserData = await createdUser.save()
+        res.status(200).json(newUserData)
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ "message": "internal server error" })
+    }
 }
 export const login = (req, res) => {
     res.send("login Page")

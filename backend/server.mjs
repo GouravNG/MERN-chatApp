7e5a49667e52e9a2345fa64dc059db306eeba0ae
Gourav <gouravgunaga555@gmail.com>
2024-03-20 21:14:39 +0530

    console to 500 handler

--- backend/server.mjs
+++ backend/server.mjs
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
 })
 app.use((err, req, res, next) => {
     res.send("500")
+    console.log(err)
 })
 app.listen(PORT, () => {
     console.log(`Server runnig on PORT ${PORT}...`)

103630ce5f4485f1f379554340e7ae92445cfeb4
Gourav <gouravgunaga555@gmail.com>
2024-03-23 18:59:04 +0530

    added the getAllusers

--- backend/controllers/userController.mjs
+++ backend/controllers/userController.mjs
@@ -1,3 +1,12 @@
-export const getUsers = (req, res) => {
-    res.send("getting all users")
+import { User } from "../models/userModel.mjs"
+
+export const getUsers = async (req, res) => {
+    try {
+        const loggeduser = req.sender._id
+        const allUser = await User.find({})
+        res.status(200).json(allUser)
+    } catch (error) {
+        console.log("error in user controller", error)
+        res.status(500).json({ error: "Internal server Error" })
+    }
 }

c2a3f6f8b3f43ec3e42cced9e3d06c88029c34e7
Gourav <gouravgunaga555@gmail.com>
2024-03-25 22:04:42 +0530

    Login Page inprogress done for the day

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { LoginPage } from './pages/loginPage/loginPage'
 
 function App() {
   return (
     <>
-     <h1>Welcome to chat app</h1>
+     <LoginPage/>
     </>
   )
 }

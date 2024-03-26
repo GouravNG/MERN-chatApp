899bb1da73e38cd11d2108ecd5f95608dfa66aca
Gourav <gouravgunaga555@gmail.com>
2024-03-26 21:49:30 +0530

    Added the signup page part1 and

---
+++ frontend/src/pages/signupPage/signUp.jsx
@@ -0,0 +1,26 @@
+import "./signUp.css"
+export const SingUp=()=>{
+    return(
+        <div className="pageBody">
+           <div className="loginBlock">
+            <div className="loginHeading"><h1>Sign Up</h1></div>
+            <div className="formDiv">
+                <form action="" method="post">
+                    <div className="inputField"><input type="text" placeholder="Full Name" /></div>
+                    <div className="inputField"><input type="text" placeholder="Username" /></div>
+                    <div className="inputField"><input type="password" placeholder="Password" /></div>
+                    <div className="inputField"><input type="password" placeholder="Confirm New Passoword" /></div>
+                    <div className="inputField">
+                    <input type="radio" id="male" name="gender" value="male" />
+                    <label htmlFor="gender">Male</label>
+                    <input type="radio" id="female" name="gender" value="female" />
+                    <label htmlFor="gender">Female</label>
+                    </div>
+                    <button>SUBMIT</button>
+                </form>
+                <p>Already a user? click here</p>
+            </div>
+           </div>
+        </div>
+    )
+}

0f1645e9e0efa92ff9692fc0718ef9d7e3231adc
Gourav <gouravgunaga555@gmail.com>
2024-04-04 15:46:02 +0530

    updated the code to give id and name on click and used dummy data to remap the code

--- frontend/src/components/chatlist.jsx
+++ frontend/src/components/chatlist.jsx
@@ -1,4 +1,24 @@
 import userPic from "../assets/49.png"
+import userData from "../getUser.json"
+
+const EachChat = ({ values }) => {
+    const { _id, fullName, profilePic, gender } = values;
+    const handleClick = (e) => {
+      const userId = e.currentTarget.getAttribute('userid');
+      console.log(e.target.innerText)
+      console.log(userId);
+    };
+  
+    return (
+      <>
+        <li userid={_id} onClick={handleClick}>
+          <img src={profilePic} alt={`${gender}`} width="60px" />
+          <h2>{fullName}</h2>
+        </li>
+      </>
+    );
+  };
+
 export const Chatlists=()=>{
     return(
     <>
@@ -7,42 +27,11 @@ export const Chatlists=()=>{
         </div>
         <div className="chat-lists">
             <ul>
-                <li>
-                    <img src={userPic} alt="userImage" width="60px"/>
-                    <h2>UserName</h2>
-                </li>
-                <li>
-                    <img src={userPic} alt="userImage" width="60px"/>
-                    <h2>UserName</h2>
-                </li>
-                <li>
-                    <img src={userPic} alt="userImage" width="60px"/>
-                    <h2>UserName</h2>
-                </li>
-                <li>
-                    <img src={userPic} alt="userImage" width="60px"/>
-                    <h2>UserName</h2>
-                </li>
-                <li>
-                    <img src={userPic} alt="userImage" width="60px"/>
-                    <h2>UserName</h2>
-                </li>
-                <li>
-                    <img src={userPic} alt="userImage" width="60px"/>
-                    <h2>UserName</h2>
-                </li>
-                <li>
-                    <img src={userPic} alt="userImage" width="60px"/>
-                    <h2>UserName</h2>
-                </li>
-                <li>
-                    <img src={userPic} alt="userImage" width="60px"/>
-                    <h2>UserName</h2>
-                </li>
-                <li>
-                    <img src={userPic} alt="userImage" width="60px"/>
-                    <h2>UserName</h2>
-                </li>
+                {
+                    userData.map((i)=>{
+                        return <EachChat key={i._id} values={i}/> 
+                    })
+                }
             </ul>
         </div>
         
